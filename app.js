89d28d9ee267a3f810a94e7e60bd1f8677b87ced
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-06-24 10:30:28 +0500

    12 hour format

--- app.js
+++ app.js
@@ -2,31 +2,57 @@
 function clock(){
     const today = new Date();
 
-    const hours = today.getHours()% 12 || 12;
+    const hours =today.getHours();
     const minutes =today.getMinutes();
    const seconds= today.getSeconds();
    let period ="AM";
-   
+
+
+
+
+   if(hours >=12 ){
+    period="PM"
+   }
+   if(hours>12){
+    hours=hours-12;
+   }
+   if(hours<10){
+    hours="0"+hours;
+   }
+   if(minutes<10){
+    minutes="0"+minutes;
+   }
+   if(seconds<10){
+    seconds="0"+seconds;
+   }
+
+//    message add
+    if(hours<12){
+    method="Good Morning"
+    }
+    if(hours>= 12 && hours<=17){
+    method="Good Afternoon"}
+    if(hours>= 17 && hours<=24){
+    method="Good Evening"}
    
 
-   if(hours >12 ){
-    period="PM"
-   }else{
-    period= "AM"
-   }
+
+
+
  
-  
+ 
 
 
    document.querySelector(".hours").innerHTML = hours;
    document.querySelector(".minutes").innerHTML = minutes;
    document.querySelector(".seconds").innerHTML = seconds;
    document.querySelector(".period").innerHTML = period;
+   document.querySelector(".method").innerHTML = method;
 
   
 }
 
-let updateclock = setInterval(clock, 1000);
+let updateclock = setInterval(clock,1000);
 
 
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
